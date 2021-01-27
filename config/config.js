9483f8725a3cc1c3183a3555240202debb0bdfc9
acbasoco17 <36174781+acbasoco17@users.noreply.github.com>
2021-01-26 19:14:24 -0500

    Fixing bug part 2

--- config/config.js
+++ config/config.js
@@ -36,9 +36,7 @@ var config = {
 			config: {
 				  effect: "flip",
 					welcome_message: "hello world"
-				}
-			/* show_notification: function("Hello World"){}
-		}, */
+				},
 		{
 			module: "updatenotification",
 			position: "top_bar"
