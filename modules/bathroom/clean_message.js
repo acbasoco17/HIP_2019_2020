248aa99b5079f3d25972349150f4f9ab68265fd7
ana <acbasoco17@gmail.com>
2021-03-17 09:31:29 -0400

    Create default part of clean_message module

--- modules/bathroom/clean_message.js
+++ modules/bathroom/clean_message.js
@@ -1,28 +1,33 @@
 Module.register("clean_message", {
-  getDom: (function () {
-    var firebase = require("firebase");
-    const config = {
-      apiKey: "AIzaSyCBYsN7VD4yYAQy-6lK5ug1GLZE63uZxyo",
-      authDomain: "magic-mirror-305401.firebaseapp.com",
-      databaseURL: "https://magic-mirror-305401-default-rtdb.firebaseio.com",
-      projectId: "magic-mirror-305401",
-      storageBucket: "magic-mirror-305401.appspot.com",
-      messagingSenderId: "128949355841",
-      appId: "1:128949355841:web:0ed3a49f49595815664dc0",
-      measurementId: "G-NEBVFJQNL4"
-    };
-    firebase.initializeApp(config);
-    var last;
-    var dbRefObject = firebase.database().ref().child("Log");
-    dbRefObject.once("child_added").then(function(snapshot){
-      var date = snapshot.child("Date").val()
-      var time = snapshot.child("Time").val()
-      var name = snapshot.child("User").val()
+  defaults: {
+    message: "Posted";
+  },
+  getDom: function() {
+    (function () {
+      var firebase = require("firebase");
+      const config = {
+        apiKey: "AIzaSyCBYsN7VD4yYAQy-6lK5ug1GLZE63uZxyo",
+        authDomain: "magic-mirror-305401.firebaseapp.com",
+        databaseURL: "https://magic-mirror-305401-default-rtdb.firebaseio.com",
+        projectId: "magic-mirror-305401",
+        storageBucket: "magic-mirror-305401.appspot.com",
+        messagingSenderId: "128949355841",
+        appId: "1:128949355841:web:0ed3a49f49595815664dc0",
+        measurementId: "G-NEBVFJQNL4"
+      };
+      firebase.initializeApp(config);
+      var last;
+      var dbRefObject = firebase.database().ref().child("Log");
+      dbRefObject.once("child_added").then(function(snapshot){
+        var date = snapshot.child("Date").val()
+        var time = snapshot.child("Time").val()
+        var name = snapshot.child("User").val()
 
-      var message = "This bathroom was last cleaned on " + date + " at " + time + " by " + name + ".";
-      var wrapper = document.createElement("div");
-      wrapper.innerHTML =this.config.message;
-      return wrapper
-    });
-  }());
+        var message = "This bathroom was last cleaned on " + date + " at " + time + " by " + name + ".";
+        var wrapper = document.createElement("div");
+        wrapper.innerHTML =this.config.message;
+        return wrapper
+      });
+    }());
+  }
 });
