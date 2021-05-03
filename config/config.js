7bf83a0f40c6d6230d43c0255c67151be123fd5e
ana <acbasoco17@gmail.com>
2021-05-03 14:53:10 -0400

    Change elements part 7

--- config/config.js
+++ config/config.js
@@ -94,7 +94,7 @@ var config = {
 		{
 		module: 'MMM-PushBulletNotifications',
 		header: 'Notifications',
-		position: 'top_center',	// This can be any of the regions.
+		position: 'top',	// This can be any of the regions.
 		config: {
 			// See 'Configuration options' for more information.
 			accessToken: "o.bzToNHGXJcPuJxBFROdPz3qjSY0BmH97", //PushBullet API Access Token
